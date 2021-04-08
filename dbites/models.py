8270bd5e76e9f523c622e13354ffce66c52f8e0b
Niraj Gurung <ngurung@unomaha.edu>
2021-04-08 16:28:04 -0500

    db

---
+++ dbites/models.py
@@ -0,0 +1,27 @@
+from django.utils import timezone
+from django.db import models
+
+
+# Create your models here.
+class Customer(models.Model):
+    cust_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=50)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def created(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.updated_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.cust_name)
